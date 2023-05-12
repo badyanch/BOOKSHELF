8c7d22e3bd9044e68e43f9c6a0821ab9603e4de0
Nazar Marchenko <109473164+MarchenkoNaz@users.noreply.github.com>
2023-05-12 11:26:06 +0300

    authorization

--- src/js/firebase.js
+++ src/js/firebase.js
@@ -1,6 +1,7 @@
 import { initializeApp } from 'firebase/app'
-import { getFirestore, collection, getDocs, doc } from 'firebase/firestore'
-const user = document.querySelector('.user')
+import { getFirestore, collection, getDocs } from 'firebase/firestore'
+import { getAuth } from 'firebase/auth';
+
 
 const firebaseConfig = {
 	apiKey: "AIzaSyCMJ4oS1RcIdUOr4jrAkCF8F7w-W-dX16A",
@@ -15,17 +16,18 @@ const firebaseConfig = {
 initializeApp(firebaseConfig)
 //init services
 const db = getFirestore()
+export const auth = getAuth()
 //colletction ref
-const colRef = collection(db, 'users')
+export const colRef = collection(db, 'users')
 //get collection data
 
 export const dataFireBase = getDocs(colRef)//get all docs from collection
 	.then((snapshot) => {
 		let users = []
 		snapshot.docs.forEach((doc) => {
-			users.push({ ...doc.data(), id: doc.id })
+			users.push({ ...doc.data(), id: doc.id });
 		})
-
+		console.log(users);
 	})
 	.catch(error => console.log(error.message))
 
