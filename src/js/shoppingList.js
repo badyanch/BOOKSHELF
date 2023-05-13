d946c74c7603265abb17311f7287085790e36e96
Franklinshtein <98875248+Franklinshtein@users.noreply.github.com>
2023-05-13 21:06:48 +0200

    beta 1.0

--- src/js/shoppingList.js
+++ src/js/shoppingList.js
@@ -1,111 +1,93 @@
-const shoppingListContainer = document.querySelector(
-    '.shopping-list-empty-page'
-  );
-  if (localStorage.getItem('shoppingList')) {
-    const shoppingListJSON = localStorage.getItem('shoppingList');
-    let shoppingList = JSON.parse(shoppingListJSON);
-  
-    const shoplistBooks = shoppingList.map(makeShoplistMarkup);
-  
-    // CreateMarkup
-  
-    function makeShoplistMarkup(shoppingList) {
-      const { bookImg, author, listName, description, title, buyLinks, id } =
-        shoppingList;
-  
-      const shoplistBookContainer = document.createElement('div');
-      shoplistBookContainer.classList.add('shoplist-book-container');
-      shoplistBookContainer.dataset.id = id;
-  
-      const shoplistMarkup = `<img src="${bookImg}" class="shoplist-book-img">
-          <div class="shoplist-desc-container">
-            <h4 class="shoplist-book-title">${title}</h4>
-            <p class="shoplist-book-genre">${listName}</p>
-            <p class="shoplist-book-description">${description}</p>
-             <p class="shoplist-book-author">${author}</p>
-          </div>
-          <div class="shoplist-icons">
-      <ul class="shoplist-icons-list">
-        <li class="shoplist-icons-li">
-          <a href="${buyLinks[0].url}" target="blank">
-            <div class="shoplist-icon-amazon"></div>
-          </a>
-        </li>
-        <li class="shoplist-icons-li">
-          <a href="${buyLinks[2].url}" target="blank">
-            <div class="shoplist-icon-barnesAndNoble"></div>
-          </a>
-        </li>
-        <li class="shoplist-icons-li">
-          <a href="${buyLinks[4].url}" target="blank">
-            <div class="shoplist-icon-bookshop"></div>
-          </a>
-        </li>
-      </ul>
-    </div>
-          <div class="shoplist-trash"></div>
-  `;
-  
-      shoplistBookContainer.innerHTML = shoplistMarkup;
-  
-      return shoplistBookContainer;
-    }
-  
-    // Create new container and replace markup
-  
-    const newShoppingListContainer = document.createElement('div');
-    newShoppingListContainer.classList.add('shopping-list__container');
-    shoplistBooks.forEach(book => {
-      newShoppingListContainer.appendChild(book);
-    });
-  
-    shoppingListContainer.replaceWith(newShoppingListContainer);
-  
-    // Add trash to each element
-  
-    const shoplistTrash = document.querySelectorAll('.shoplist-trash');
-    shoplistTrash.forEach(trash => {
-      trash.addEventListener('click', removesBookFromShoppingList);
-    });
-  
-    // Update Local Storage
-  
-    function removesBookFromShoppingList(event) {
-      const id = event.target.closest('.shoplist-book-container').dataset.id;
-      shoppingList = shoppingList.filter(book => book.id !== id);
-      localStorage.setItem('shoppingList', JSON.stringify(shoppingList));
-      newShoppingListContainer.removeChild(
-        event.target.closest('.shoplist-book-container')
-      );
-  
-      // Fix updating on the page
-  
-      if (shoppingList.length === 0) {
-        newShoppingListContainer.replaceWith(shoppingListContainer);
-        paginationContainer.classList.add('pagination-hidden');
-      }
-    }
-  
-    function updateBookOnStorage() {
-      localStorage.setItem('shoppingList', JSON.stringify(shoppingList));
-    }
-  
+// Get the shopping list container element
+const shoppingListContainer = document.querySelector('.shopping-list__container');
+
+// Check if shoppingList exists in localStorage
+if (localStorage.getItem('shoppingList')) {
+  // Retrieve the shoppingList from localStorage
+  let shoppingList = JSON.parse(localStorage.getItem('shoppingList'));
+
+  // Create markup for each book in the shoppingList
+  const shoplistBooks = shoppingList.map(makeShoplistMarkup);
+
+  // Create markup for a single book
+  function makeShoplistMarkup({ bookImg, author, listName, description, title, buyLinks, id }) {
+    const shoplistBookContainer = document.createElement('div');
+    shoplistBookContainer.classList.add('shoplist-book-container');
+    shoplistBookContainer.dataset.id = id;
+
+    const shoplistMarkup = `
+      <img src="${bookImg}" class="shoplist-book-img">
+      <div class="shoplist-desc-container">
+        <h4 class="shoplist-book-title">${title}</h4>
+        <p class="shoplist-book-genre">${listName}</p>
+        <p class="shoplist-book-description">${description}</p>
+        <p class="shoplist-book-author">${author}</p>
+      </div>
+      <div class="shoplist-icons">
+        <ul class="shoplist-icons-list">
+          <li class="shoplist-icons-li">
+            <a href="${buyLinks[0].url}" target="_blank">
+              <div class="shoplist-icon-amazon"></div>
+            </a>
+          </li>
+          <li class="shoplist-icons-li">
+            <a href="${buyLinks[2].url}" target="_blank">
+              <div class="shoplist-icon-ibook"></div>
+            </a>
+          </li>
+          <li class="shoplist-icons-li">
+            <a href="${buyLinks[4].url}" target="_blank">
+              <div class="shoplist-icon-bookshop"></div>
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div class="shoplist-trash"></div>
+    `;
+
+    shoplistBookContainer.innerHTML = shoplistMarkup;
+
+    return shoplistBookContainer;
+  }
+
+  // Create a new container and replace the existing one
+  const newShoppingListContainer = document.createElement('div');
+  newShoppingListContainer.classList.add('shopping-list__container');
+  shoplistBooks.forEach(book => {
+    newShoppingListContainer.appendChild(book);
+  });
+
+  shoppingListContainer.replaceWith(newShoppingListContainer);
+
+  // Add event listener to each trash icon
+  newShoppingListContainer.addEventListener('click', handleTrashIconClick);
+
+  // Remove a book from the shopping list
+  function removesBookFromShoppingList(id) {
+    shoppingList = shoppingList.filter(book => book.id !== id);
+    updateBookOnStorage();
+
+    // Fix updating on the page
     if (shoppingList.length === 0) {
-      if (!document.querySelector('.shopping-list-container')) {
-        document.body.appendChild(newShoppingListContainer);
-      }
       newShoppingListContainer.replaceWith(shoppingListContainer);
+    } else {
+      const bookContainerToRemove = newShoppingListContainer.querySelector(`[data-id="${id}"]`);
+      bookContainerToRemove.remove();
     }
-  
-//     // Pagination
-  
-//     const paginationContainer = document.querySelector('.pagination-container');
-  
-//     if (paginationContainer) {
-//       if (shoppingList.length === 0) {
-//         paginationContainer.classList.add('pagination-hidden');
-//       } else {
-//         paginationContainer.classList.remove('pagination-hidden');
-//       }
-//     }
-//   }
+  }
+
+  // Handle click event on trash icon
+  function handleTrashIconClick(event) {
+    if (event.target.classList.contains('shoplist-trash')) {
+      const bookContainer = event.target.closest('.shoplist-book-container');
+      const id = bookContainer.dataset.id;
+      removesBookFromShoppingList(id);
+    }
+  }
+
+  // Update Local Storage
+  function updateBookOnStorage() {
+    localStorage.setItem('shoppingList', JSON.stringify(shoppingList));
+  }
+
+// Hide/show pagination container
