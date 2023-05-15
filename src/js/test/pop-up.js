c397311ed5c9e0e369229b0e68742f7b5cf24031
NataliaHal <118615900+NataliaHal@users.noreply.github.com>
2023-05-15 09:07:22 +0200

    commit 15.05.09.07

--- src/js/test/pop-up.js
+++ src/js/test/pop-up.js
@@ -1,23 +1,42 @@
 import { fetchBooksForID } from '../API';
 
+
+
+
+
 function createMarkupBooks(category) {
-   const { _id, list_name, book_image, title, author } = category
+  
+  const { _id, list_name, book_image, title, author } = category
+  
   const markup = 
  
   `<div class="data-book">
-<img src="${book_image}" alt="${list_name}">
+<img src="${book_image}" alt="${list_name}" class="book-image">
 <div class="data-book__info">
 <h2 class="data-book__title">${title}</h2>
 <h3 class="data-book__subtitle">${author}</h3>
 <p class="data-book__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. In doloremque soluta est possimus, porro quidem minus, iste accusantium ut impedit officiis corporis placeat unde neque. Molestias expedita eum nesciunt illum.
 </p>
+<ul  class="logo-shops-link">
+  <li class="logo-shops-title">
+<img src="/src/images/shoppinglist/amazon-b-w.png" alt="логотип магазина">
+  </li>
+   <li class="logo-shops-link">
+<img src="/src/images/shoppinglist/ibook-b-w.png" alt="логотип магазина">
+   </li>
+    <li class="logo-shops-link">
+<img src="/src/images/shoppinglist/bookshop.png" alt="логотип магазина">
+    </li>
+</ul>
 </div>
-<button type="button" class="button-delete">delete</button>
+<button type="button" class="button-delete">Add to shopping list</button>
 `;  
-modalWindow.insertAdjacentHTML("beforeend", markup)
+  modalWindow.insertAdjacentHTML("beforeend", markup)
+  
+  
  
 }
- 
+
 const ul = document.querySelector('.books-cards__list');
 
 ul.addEventListener('click', onClickBookCard)
@@ -31,6 +50,7 @@ async function onClickBookCard(event) {
   }
 
 }
+
 const modalWindow = document.querySelector(".book-info");
 
 async function onClickBookCard(event) {
@@ -45,6 +65,8 @@ async function onClickBookCard(event) {
   // const markup = await createMarkupBooks(fetchAPI.data);
 
 }
+
+
 
 
 // <div class="book-shops-links">
