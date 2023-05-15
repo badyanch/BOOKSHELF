1f3e9439d10e901465b72f407d238e6ae816377d
NataliaHal <118615900+NataliaHal@users.noreply.github.com>
2023-05-15 21:02:23 +0200

    1111111111

--- src/js/test/pop-up.js
+++ src/js/test/pop-up.js
@@ -1,76 +1,98 @@
+import { Notify } from 'notiflix';
 import { fetchBooksForID } from '../API';
-
-
-
-
+import { saveInStorage } from '../shopping-card-storage';
+import { getStorageItem } from '../shopping-card-storage';
+import { STORAGE_KEY } from '../shopping-card-storage';
+import { newArray } from '../shopping-card-storage';
 
 function createMarkupBooks(category) {
-  
-  const { _id, list_name, book_image, title, author } = category
-  
-  const markup = 
- 
-  `<div class="data-book">
-<img src="${book_image}" alt="${list_name}" class="book-image">
+  const { _id, list_name, book_image, title, author, buy_links } = category;
+  const socialMarkup = buy_links
+    .map(item => {
+      const a = './images/png/amazon-shop.png';
+
+      const newSocialMarkup = `<div class="book-shops-links">
+  <a href="${item.url}" >${item.name}<svg>
+    <use src="../../images/icons#icon-Amazon_logo.svg"></use>
+</svg></a>
+</div>`;
+      return newSocialMarkup;
+    })
+    .join('');
+  const markup = `<div class="data-book">
+<img src="${book_image}" alt="${list_name}">
 <div class="data-book__info">
 <h2 class="data-book__title">${title}</h2>
 <h3 class="data-book__subtitle">${author}</h3>
 <p class="data-book__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. In doloremque soluta est possimus, porro quidem minus, iste accusantium ut impedit officiis corporis placeat unde neque. Molestias expedita eum nesciunt illum.
 </p>
-<ul  class="logo-shops-link">
-  <li class="logo-shops-title">
-<img src="/src/images/shoppinglist/amazon-b-w.png" alt="логотип магазина">
-  </li>
-   <li class="logo-shops-link">
-<img src="/src/images/shoppinglist/ibook-b-w.png" alt="логотип магазина">
-   </li>
-    <li class="logo-shops-link">
-<img src="/src/images/shoppinglist/bookshop.png" alt="логотип магазина">
-    </li>
-</ul>
 </div>
-<button type="button" class="button-delete">Add to shopping list</button>
-`;  
-  modalWindow.insertAdjacentHTML("beforeend", markup)
-  
-  
- 
+<button type="button" class="button-js-storage" data-favoriteid='${_id}'>Add to shopping list</button>
+`;
+  modalWindow.insertAdjacentHTML('beforeend', markup);
+  modalWindow.insertAdjacentHTML('beforeend', socialMarkup);
+}
+
+let localData = getStorageItem(STORAGE_KEY);
+let secondArray;
+
+if (!localData) {
+  secondArray = [];
+} else {
+  secondArray = newArray.concat(localData);
 }
 
 const ul = document.querySelector('.books-cards__list');
 
-ul.addEventListener('click', onClickBookCard)
+ul.addEventListener('click', onClickBookCard);
+
+const modalWindow = document.querySelector('.book-info');
 
 async function onClickBookCard(event) {
-  const test = await event.target;
-
-  if (event.target.nodeName !== "LI") {
-    return
-  } else { 
-  }
-
-}
-
-const modalWindow = document.querySelector(".book-info");
-
-async function onClickBookCard(event) {
+  modalWindow.innerHTML = '';
   const li = event.target.closest('.category-books__item');
   if (!li) {
     return;
   }
   const favoritBookId = li.dataset.id;
-  const fetchAPI = await fetchBooksForID(favoritBookId);
-  createMarkupBooks(fetchAPI.data)
-  // console.log("ответ", fetchAPI);
-  // const markup = await createMarkupBooks(fetchAPI.data);
 
+  try {
+    const fetchAPI = await fetchBooksForID(favoritBookId);
+
+    createMarkupBooks(fetchAPI.data);
+
+    const saveBookInStorageButton =
+      document.querySelector('.button-js-storage');
+
+    if (secondArray.includes(saveBookInStorageButton.dataset.favoriteid)) {
+      saveBookInStorageButton.textContent = 'remove from the shopping list';
+    }
+
+    saveBookInStorageButton.addEventListener('click', () => {
+      const checkStorage = getStorageItem(STORAGE_KEY);
+
+      if (saveBookInStorageButton.textContent === 'Add to shopping list') {
+        if (secondArray.includes(saveBookInStorageButton.dataset.favoriteid)) {
+          return;
+        }
+
+        secondArray.push(saveBookInStorageButton.dataset.favoriteid);
+        saveInStorage(STORAGE_KEY, secondArray);
+        saveBookInStorageButton.textContent = 'remove from the shopping list';
+      } else if (
+        saveBookInStorageButton.textContent === 'remove from the shopping list'
+      ) {
+        const test = checkStorage.indexOf(
+          saveBookInStorageButton.dataset.favoriteid
+        );
+        secondArray.splice(test, 1);
+
+        saveInStorage(STORAGE_KEY, secondArray);
+        saveBookInStorageButton.textContent = 'Add to shopping list';
+        return;
+      }
+    });
+  } catch (error) {
+    Notify.failure(error.message);
+  }
 }
-
-
-
-
-// <div class="book-shops-links">
-//   <a href="${url}" ><img src="../../images/save to Ukraine/image 1.png" alt="${name}"></a>
-//    <a href=""><img src="" alt=""></a>
-//    <a href=""><img src="" alt=""></a>
-// </div>`;
