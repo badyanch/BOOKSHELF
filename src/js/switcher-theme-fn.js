2741a1abd4e7b0a961c35249dcdc3ac4b065df66
AnetRo <beatrice.ccq@gmail.com>
2023-05-16 01:16:58 +0300

    not at all

--- src/js/switcher-theme-fn.js
+++ src/js/switcher-theme-fn.js
@@ -1,7 +1,3 @@
-
-
-
-
 const checkbox = document.querySelector(".switcher-theme__checkbox");
 
 checkbox.addEventListener("click", onSwitcherClick);
@@ -39,50 +35,3 @@ export function onSwitcherClick() {
     }  
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const checkbox = document.querySelector(".switcher-theme__checkbox");
-
-// checkbox.addEventListener("click", onSwitcherClick);
-
-// export function onSwitcherClick() {
-
-//   if (this.checked) {
-
-//     const css = "body {background-color: #202024; color: white;}";
-//     const element = document.createElement("style");
-//     element.setAttribute("type", "text/css");
-//     element.appendChild(document.createTextNode(css));
-//     element.classList.add("style-class");
-//     document.head.appendChild(element);
-    
-//   } else {
-//     const element = document.querySelector(".style-class");
-    
-//     if (element) {
-//       element.remove()
-//     }  
-//   }
-// }
