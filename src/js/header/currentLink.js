cff0031eea512f12b2281914a54263a9c02a1708
Bohdan Zlotko <bohdanzlotko@gmail.com>
2023-05-16 22:10:41 +0300

    add fix

--- src/js/header/currentLink.js
+++ src/js/header/currentLink.js
@@ -1,25 +1,12 @@
 onCurrentLink();
-
 function onCurrentLink() {
   const pathName = window.location.pathname;
-
-  if (pathName.endsWith('/')) {
-    const links = document.querySelectorAll(`[href="${pathName}index.html"]`);
-
-    links.forEach(link => {
-      if (link.classList.contains('page-nav__link')) {
-        link.classList.add('page-nav__link--current');
-      }
-    });
-
-    return;
-  }
-
-  const links = document.querySelectorAll(`[href="${pathName}"]`);
-
+  const links = document.querySelectorAll('.page-nav__link');
   links.forEach(link => {
-    if (link.classList.contains('page-nav__link')) {
+    if (link.getAttribute('href') === pathName || link.getAttribute('href') === pathName + 'index.html') {
       link.classList.add('page-nav__link--current');
+    } else {
+      link.classList.remove('page-nav__link--current');
     }
   });
 }
