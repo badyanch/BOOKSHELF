51323a037b2e5deb72c2f12df46609f7b14e0ef6
badyanch <106490374+badyanch@users.noreply.github.com>
2023-05-16 19:12:09 +0000

    Deploying to gh-pages from @ badyanch/BOOKSHELF@acb8728ac1470c57db2d99cba598628fcd62982a 🚀

---
+++ ShoppingPage.c47af1c3.js
@@ -0,0 +1,2 @@
+function onCurrentLink(){const n=window.location.pathname;document.querySelectorAll(".page-nav__link").forEach((e=>{e.getAttribute("href")===n||e.getAttribute("href")===n+"index.html"?e.classList.add("page-nav__link--current"):e.classList.remove("page-nav__link--current")}))}onCurrentLink();
+//# sourceMappingURL=ShoppingPage.c47af1c3.js.map
