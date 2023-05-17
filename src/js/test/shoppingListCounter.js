6eb9cea13d90dc84e5dcaa34bdf0e4fb76105c93
Alexey Luzanchuk <Luzanchuk123@gmail.com>
2023-05-18 01:07:24 +0300

    add black theme to modal window

--- src/js/test/shoppingListCounter.js
+++ src/js/test/shoppingListCounter.js
@@ -2,18 +2,20 @@
 
 
 // function getNumberFromStorageNDraw() {
-//     const savedBook = localStorage.getItem('') || '';
+//     const savedBook = localStorage.getItem('currentUser') || '';
 //     if (savedBook === '') {
 //         return console.log('There is no books in storage');
 //     }
 //     const parsedBooks = JSON.parse(savedBook);
-//     listLength = parsedBooks.length;
-//     const basketThubm = document.querySelector('.page-nav__cart-number');
-//     console.log(basketThubm);
-//     const markup = `<div class="header-wishlist-counter-thumb">${listLength}</div>`;
-//     basketThubm.classList.remove('.visually-hidden');
-//     basketThubm.textContent = listLength;
-//     basketThubm.insertAdjacentHTML('beforeend', markup);
+//     console.log(parsedBooks);
+//     // const listLength = parsedBooks.length;
+    
+//     // const basketThubm = document.querySelector('.page-nav__cart-number');
+// //     console.log(basketThubm);
+// //     const markup = `<div class="header-wishlist-counter-thumb">${listLength}</div>`;
+// //     basketThubm.classList.remove('.visually-hidden');
+// //     basketThubm.textContent = listLength;
+// //     basketThubm.insertAdjacentHTML('beforeend', markup);
 // }
 
 
