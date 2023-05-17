a5c8d6fd585720a3a8780ec02a63ad76b31842f7
badyanch <106490374+badyanch@users.noreply.github.com>
2023-05-17 11:57:32 +0000

    Deploying to gh-pages from @ badyanch/BOOKSHELF@5936c06165250f506091cc8a70c3b1e195e1201d 🚀

--- ShoppingPage.0958e5e4.js
+++
@@ -1,2 +0,0 @@
-const btnEl=document.querySelector(".js-user");btnEl&&btnEl.addEventListener("click",(()=>{window.innerWidth<768||btnEl.classList.toggle("user--is-active-btns-box")}));
-//# sourceMappingURL=ShoppingPage.0958e5e4.js.map
