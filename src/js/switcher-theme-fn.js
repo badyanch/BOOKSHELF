9b20e16a4bf0584a6e725fac9ebe605261b784ed
Alexey Luzanchuk <Luzanchuk123@gmail.com>
2023-05-18 14:36:11 +0300

    cmt

--- src/js/switcher-theme-fn.js
+++ src/js/switcher-theme-fn.js
@@ -1,23 +1,42 @@
-const checkbox = document.querySelector(".switcher-theme__checkbox");
+// window.onload = checkTheme();
 
-checkbox.addEventListener("click", onSwitcherClick);
 
-export function onSwitcherClick() {
 
-  if (this.checked) {
+// function checkTheme() {
+//   const checkbox = document.getElementById("checkbox")
+// const checkboxLabel = document.querySelector('.checkbox-label')
 
-    const css = "body {background-color: #202024; color: white;}";
-    const element = document.createElement("style");
-    element.setAttribute("type", "text/css");
-    element.appendChild(document.createTextNode(css));
-    element.classList.add("style-class");
-    document.head.appendChild(element);
-    
+
+//   const localStorageTheme = localStorage.getItem('theme');
+
+//   if (localStorageTheme !== null && localStorageTheme === 'dark-theme') {
+//     document.body.className = localStorageTheme;
+//     checkboxLabel.classList.toggle('checkbox-label--dark-theme');
+//     // checkbox.checked = true;
+//   }else if (localStorageTheme !== null && localStorageTheme === 'light-theme') {
+//     document.body.className = 'localStorageTheme';
+//     checkboxLabel.classList = 'checkbox-label';
+//   }
+// }
+
+
+const checkboxLabel = document.querySelector('.checkbox-label')
+const checkbox = document.getElementById("checkbox")
+
+checkbox.checked = false;
+
+
+
+checkbox.addEventListener("change", () => {
+  console.log(checkbox.checked);
+  if (!checkbox.checked) {
+    document.body.classList.toggle("dark-theme")
+    localStorage.setItem('theme', 'light-theme');
+    checkboxLabel.classList.toggle('checkbox-label--dark-theme');
   } else {
-    const element = document.querySelector(".style-class");
-    
-    if (element) {
-      element.remove()
-    }  
+    document.body.classList.toggle("dark-theme")
+    // document.body.classList.toggle("light-theme")
+    checkboxLabel.classList.toggle('checkbox-label--dark-theme');
+    localStorage.setItem('theme', 'dark-theme');
   }
-}
+})
